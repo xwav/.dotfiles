23de37a077c4e6ce53019d64fed9d5608992f5f3
Gizmo Truveauth <ddxxxxbb@gmail.com>
2022-02-18 21:43:20 +0200

    with folder 4

---
+++ installscript.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#install software and packages
+
+sudo apt update && \
+sudo apt install \
+
+libxft-dev \
+libxinerama-dev \
+libx11-dev \
+xorg \
+xserver-xorg \
+git \
+curl \
+wget \
+build-essential \
+feh \
+conky \
+lm-sensors \
+cmus \
+newsboat \
+xcompmgr \
+alsa-utils \
+acpi \
+sxiv \
+scrot \
+zsh \
+zsh-autosuggestions \
+zsh-syntax-highlighting \
+tmux \
+most \
+rlwrap \
+keepassxc \
+zathura \
+pmount
+
+#create directories 
+mkdir ~/.config 
+
+#download and build suckless tools
+cd ~/.config
+git clone https://github.com/xwav/suckless && \
+cd ~/.config/dwm && make && make clean install 
+cd ~/.config/dmenu && make && make clean install
+cd ~/.config/st && make && make clean install
+
+#download and build nvim
+cd ~/.config
+git clone https://github.com/neovim/neovim
+cd ~/.config/neovim && git checkout stable 
+make && make install
+
+#download and install cht.sh
+curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
+chmod +x /usr/local/bin/cht.sh
+
+# pull bare dotfile repo
+git clone \
+    --separate-git-dir=$HOME/.dotfiles \
+    git@github.com:xwav/.dotfiles.git 
+
+# download script for running bashmount
+cd /usr/local/bin
+git clone https://raw.githubusercontent.com/jamielinux/bashmount/master/bashmount
+
+#install GoogleDrive ocamlfuse
+apt install \
+software-properties-common \
+dirmngr \
+gnupg-agent
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
+echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list >/dev/null
+
+apt update
+apt install \
+google-drive-ocamlfuse
+
+#make zsh a default shell
+chsh -s $(which zsh) $USER
+
+
+
+
+
+
