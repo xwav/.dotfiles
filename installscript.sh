353d0a6912520633017cd6b11ebbb9a78e6f649a
Gizmo Truveauth <ddxxxxbb@gmail.com>
2022-04-28 21:24:54 +0300

    dsadsa

--- installscript.sh
+++ installscript.sh
@@ -38,22 +38,20 @@ calcurse
 
 
 #download and build suckless tools
-cd ~/.config
-git clone https://github.com/xwav/suckless && \
-cd ~/.config/suckless/dwm && make && make clean install 
-cd ~/.config/suckless/dmenu && make && make clean install
-cd ~/.config/suckless/st && make && make clean install
+
+git clone https://github.com/xwav/suckless $HOME/.config/suckless && \
+cd $HOME/.config/suckless/dwm && make && make clean install 
+cd $HOME/.config/suckless/dmenu && make && make clean install
+cd $HOME/.config/suckless/st && make && make clean install
 
 #download and build nvim
-cd ~/.config
-git clone https://github.com/neovim/neovim
-cd ~/.config/neovim && git checkout stable 
-make && make install
-cd ~/.config && rm -r neovim
+
+git clone https://github.com/neovim/neovim $HOME/.config/neovim
+cd $HOME/.config/neovim && git checkout stable && make && make install
+#cd ~/.config && rm -r neovim
 
 #get my bester nvim config :)
-cd ~/.config/
-git clone https://github.com/xwav/nvim.git
+git clone https://github.com/xwav/nvim.git $HOME/.config/nvim
 
 #download and install cht.sh
 curl https://cht.sh/:cht.sh | tee /usr/local/bin/cht.sh
@@ -73,30 +71,43 @@ apt update
 apt install \
 google-drive-ocamlfuse
 
-#make zsh a default shell
+#make zsh a default shelll
 chsh -s $(which zsh) $USER
 
-
 # Installing dotfiles 
-echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"' >> $HOME/.zshrc
 source ~/.zshrc
-echo ".dotfiles.git" >> $HOME/.gitignore
-git clone --bare https://www.github.com/xwav/.dotfiles.git $HOME/.dotfiles.git
-#dotfiles checkout
-#dotfiles config --local status.showUntrackedFiles no
+echo ".dotfiles" >> .gitignore
 
-#backup old config files
+git clone --bare https://www.github.com/xwav/.dotfiles.git $HOME/.dotfiles
+
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $@
-}
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+ }
 mkdir -p ~/.dotfiles-backup && \
+
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+ else
+  echo "Backing up pre-existing dot files.";
+dotfiles checkout 2>&1 | egrep "\s+\."  | awk {'print $1'} | rev | cut -d/ -f2- | rev | xargs -I{} mkdir -p .dotfiles-backup/{}
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
+
+#to init bare repo on your machine see below
+
+# git init --bare $HOME/.dotfiles.git
+# echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+# source ~/.zshrc
+# dotfiles config --local status.showUntrackedFiles no
+#
+# #with 'dotfiles' command execute same commands as with git
+# dotfiles status
+# dotfiles add .vimrc
+# dotfiles commit -m "Add vimrc"
+# dotfiles remote add origin https://www.github.com/username/repo.git
+# dotfiles push origin master
 
